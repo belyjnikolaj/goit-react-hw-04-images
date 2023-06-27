a9c8e097c584261f115658b98f78d17ba3b9946a
belyjnikolaj <117487574+belyjnikolaj@users.noreply.github.com>
2023-06-28 01:02:33 +0300

    Adds changes

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -20,26 +20,3 @@ const App = () => {
 }
 
 export default App;
-// class App extends Component {
-//   state = {
-//     searchText:'',
-//   };
-
-//   handleSearch = async (searchText) => {
-//     this.setState({ searchText });
- 
-//   };
-
-//   render() {
-//     const { searchText } = this.state;
-
-//     return (
-//       <div className={css.App}>
-//         <Searchbar handleSearch={ this.handleSearch} />
-//         <ImageGallery searchText={searchText}/>      
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
--- src/components/imageGalleryItem/ImageGalleryItem.jsx
+++ src/components/imageGalleryItem/ImageGalleryItem.jsx
@@ -27,33 +27,3 @@ const ImageGalleryItem = ({ imageUrl, alt, id, largeImageURL } ) => {
 } 
 
 export default ImageGalleryItem;
-// class ImageGalleryItem extends Component {
-    // state = { 
-    //     showModal: false,
-    // } 
-    // toggleModal = () => {
-    //     this.setState(({showModal}) => ({
-    //         showModal: !showModal
-    //     }))
-    // }
-
-//     render() { 
-//         const { id, imageUrl, alt, largeImageURL } = this.props;
-//         const {showModal} = this.state
-//         return (
-//         <li className={css['gallery-item']} key={id}>
-//             <img
-//                 className={css['gallery-item__image']}
-//                 src={imageUrl}
-//                     alt={alt}
-//                     onClick={this.toggleModal}
-//             />
-//             {showModal && <Modal onClose={this.toggleModal} largeImageURL={largeImageURL} alt={alt}>
-//                 <img src={largeImageURL} alt={alt} />                     
-//             </Modal>}                
-//         </li>
-//     );
-//     }
-// } 
-
-// export default ImageGalleryItem;
